e175de1cf17d114816f1968ee7ac1871eb5b4c0e
Fabiano <radbios@gmail.com>
2025-05-08 02:19:52 -0300

    update

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ use Firebase\JWT\Key;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/cart", [CartController::class, "index"]);
-Route::post("/cart", [CartController::class, "store"]);
-Route::get('/cart/count', [CartController::class, 'count']);
 Route::middleware("api_auth")->group(function() {
+    Route::get("/cart", [CartController::class, "index"]);
+    Route::post("/cart", [CartController::class, "store"]);
+    Route::get('/cart/count', [CartController::class, 'count']);
 });
